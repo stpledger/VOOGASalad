7f6306ae7d1f39a09f95beb283d04ceedb5ff2da
Ryan Fu <rwf11@duke.edu>
2018-04-02 20:14:33 -0400

    first commit

---
+++ data/GamePlayer/Main.java
@@ -0,0 +1,22 @@
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	
+	prviate static final String TITLE = "VoogaSalad";
+	private static Stage mainStage;
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		MainView simulation = new MainView;
+		mainStage = primaryStage;
+		primaryStage.setTitle(TITLE);
+		primaryStage.show();
+	}
+	
+}
