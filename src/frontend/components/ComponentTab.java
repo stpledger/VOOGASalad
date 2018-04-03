8911a9a0bc3af9cdc46fcef5d1adf901afc385b6
Collin Brown <cdb55@duke.edu>
2018-04-02 23:18:23 -0400

    scrollpane improved

--- src/frontend/components/ComponentTab.java
+++ src/frontend/components/ComponentTab.java
@@ -3,7 +3,11 @@ package frontend.components;
 import frontend.IDEView;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 
 /**
  * 
@@ -12,7 +16,8 @@ import javafx.scene.control.Tab;
  */
 public class ComponentTab extends Tab{
 	IDEView ideView;
-	ScrollPane scrollPane;
+	GridPane pane;
+	ScrollPane externalPane;
 	public ComponentTab(String name, IDEView v) {
 		super(name);
 		ideView = v;
@@ -22,16 +27,33 @@ public class ComponentTab extends Tab{
 	}
 	
 	private void assemble() {
-		scrollPane = new ScrollPane();
-		scrollPane.setPrefWidth(ideView.getComponentViewWidth());
-		this.setContent(scrollPane);
+		externalPane = new ScrollPane();
+		externalPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+		pane = new GridPane();
+		pane.setPrefWidth(ideView.getComponentViewWidth());
+		//TODO: Make this dynamic to handle each individual component
+		for(int i = 0; i < 9; i++) {
+		pane.add(new ComponentBox("bleh", "temp"), 0, i);
+		pane.add(new ComponentBox("bleh", "temp"), 1, i);
+		pane.add(new ComponentBox("bleh", "temp"), 2, i);
+		}
+		externalPane.setContent(pane);
+		this.setContent(externalPane);
 	}
 	/**
 	 * 
 	 * @return
 	 */
 	public Node getNode() {
-		return scrollPane;
+		return pane;
+	}
+	
+	private class ComponentBox extends Rectangle{
+		public ComponentBox(String name, String imagePath) {
+			this.setWidth(ideView.getComponentViewWidth()/3);
+			this.setHeight(ideView.getComponentViewWidth()/3);
+			this.getStyleClass().add("component-box");
+		}
 	}
 	
 }
