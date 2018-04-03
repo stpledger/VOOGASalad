1b67dda6538cc070c2ee09aa2ff0c8ad4c0ba410
yl366 <yl366@duke.edu>
2018-04-03 14:14:49 -0400

    add files

--- src/systems/Gravity.java
+++ src/systems/Gravity.java
@@ -1,21 +1,10 @@
-<<<<<<< HEAD:src/gameEngine/ECS/systems/Gravity.java
-package gameEngine.ECS.systems;
+package systems;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.List;
-
-import gameEngine.ECS.components.IComponent;
-=======
-package systems;
-
-import java.util.List;
-
 import components.IComponent;
-
-public class Gravity implements ISystem {
->>>>>>> ab308d7045b80c175976d1e4971d96daf04903bc:src/systems/Gravity.java
 
 public class Gravity implements ISystem {
 	private final String COMPONENTS_NAMES = "components_names.properties";
@@ -37,6 +26,7 @@ public class Gravity implements ISystem {
     
     @Override
     public void execute(List<IComponent> components) {
+    		for 
 
     }
 
