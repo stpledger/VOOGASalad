df90e9aec1e6765ee7becad520acc8b8fd95e293
Collin Brown <cdb55@duke.edu>
2018-04-03 17:29:06 -0400

    created levelView class

---
+++ src/frontend/components/LevelView.java
@@ -0,0 +1,19 @@
+package frontend.components;
+
+import frontend.IDEView;
+import javafx.scene.Node;
+
+public class LevelView extends ViewComponent {
+
+	public LevelView(IDEView v) {
+		super(v);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Node getNode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
