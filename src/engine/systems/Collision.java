6578c63f2b692cbd2c0bbe67be3958531d8975e6
Jack Fitzpatrick <jcf44@duke.edu>
2018-04-04 11:33:25 -0400

    Updated collision components, engine interface

--- src/engine/systems/Collision.java
+++ src/engine/systems/Collision.java
@@ -4,14 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 import engine.components.Component;
+import engine.components.Dimension;
 import engine.components.Position;
 
 public class Collision implements System {
 
 	private Map<Integer, Position> positions;
+	private Map<Integer, Dimension> dimensions;
 	
 	public Collision() {
 		positions = new HashMap<>();
+		dimensions = new HashMap<>();
 	}
 
 	public void execute(double elapsedTime) {
@@ -22,11 +25,17 @@ public class Collision implements System {
 		if(c instanceof Position) {
 			positions.put(pid, (Position) c);
 		}
+		if(c instanceof Dimension) {
+			dimensions.put(pid, (Dimension) c);
+		}
 	}
 
 	public void removeComponent(int pid) {
 		if(positions.containsKey(pid)) {
 			positions.remove(pid);
+		}
+		if(dimensions.containsKey(pid)) {
+			dimensions.remove(pid);
 		}
 	}
 	
