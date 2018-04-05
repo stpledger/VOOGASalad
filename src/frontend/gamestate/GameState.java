e952cac005c9afb60824c4ca74ebe12dd4d90579
Dylan Powers <ddp19@duke.edu>
2018-04-05 16:59:03 -0400

    Created GameState package

---
+++ src/frontend/gamestate/GameState.java
@@ -0,0 +1,26 @@
+package frontend.gamestate;
+
+/**
+ * Keeps track of the current state of the authoring environment, so that the author can save/load games dynamically. r
+ * @author dylanpowers
+ *
+ */
+public class GameState implements IGameState {
+
+	 
+	/**
+	 * This object should only be constructed once, upon initialization of the authoring environment.
+	 * It will then continue to keep track of the current state of the game by using the update method below.
+	 */
+	public GameState() {
+		
+	}
+
+	public void save() {
+		// TODO figure out how to use Java
+	}
+	
+	public void update() {
+		
+	}
+}
