0d9d1b4136e58f741be282b388b140a2b438b410
Jack Fitzpatrick <jcf44@duke.edu>
2018-04-05 15:17:17 -0400

    Updated damage system

--- src/engine/systems/HealthDamage.java
+++ src/engine/systems/HealthDamage.java
@@ -1,9 +1,12 @@
 package engine.systems;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import engine.components.Component;
+import engine.components.Damage;
 import engine.components.Health;
 import engine.components.Poison;
 
@@ -11,37 +14,41 @@ import engine.components.Poison;
 
 public class HealthDamage implements ISystem {
 	
+	public static int HEALTH_INDEX = 0;
+	public static int DAMAGE_INDEX = 1;
 	
-	private Map<Integer, Health> healthComponents;
+	private Map<Integer, List<Component>> healthComponents;
 
 	public HealthDamage() {
 		healthComponents = new HashMap<>();
 	}
 	
-	@Override
-	public void addComponent(int pid, Component h) {
-		if(h instanceof Health) {
-			healthComponents.put(pid, (Health) h);
+	public void addComponent(int pid, Map<String, Component> components) {
+		if (components.containsKey("Health") && components.containsKey("Damage")) {
+			List<Component> newComponents = new ArrayList<>();
+			newComponents.add(components.get("Health"));
+			newComponents.add(components.get("Damage"));
+			
+			healthComponents.put(pid, newComponents);
 		}
 	}
 	
-	@Override
 	public void removeComponent(int pid) {
 		if(healthComponents.containsKey(pid)) {
     		healthComponents.remove(pid);
     	}  
 	}
 	
-	public void execute(int pid, Poison p) {
-		if(healthComponents.containsKey(pid)) {
-			double change = healthComponents.get(pid).getHealth()-p.getPoison();
-		    healthComponents.get(pid).setHealth(change);
-		}
-	}
-
-	@Override
-	public void execute(double elapsedTime) {
-		// TODO Auto-generated method stub
-		
+	public void execute(double time) {
+		healthComponents.forEach((key, list) -> {
+			Health h = (Health) list.get(HEALTH_INDEX);
+			Damage d = (Damage) list.get(DAMAGE_INDEX);
+			
+			h.setHealth(h.getHealth() - d.getDamage());
+			d.decrementLife();
+			if(d.getLifetime() == 0) {
+				healthComponents.remove(h.getParentID());
+			}
+		});
 	}
 }
