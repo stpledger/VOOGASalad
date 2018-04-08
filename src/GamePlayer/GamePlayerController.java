98433d6c3f9e6d34a9495cd6f859af74a2d8326b
Ryan Fu <rwf11@duke.edu>
2018-04-08 01:11:11 -0400

    added controller class for the game player

--- src/GamePlayer/GamePlayerController.java
+++ src/GamePlayer/GamePlayerController.java
@@ -7,18 +7,21 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToolBar;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class GamePlayerView {
+public class GamePlayerController {
 	private final int WIDTH_SIZE = 600;
 	private final int HEIGHT_SIZE = 800;
 	private Scene myScene;
 	private Group group;
 	private BorderPane pane = new BorderPane();
 
-	public GamePlayerView() {
-		
+	public GamePlayerController() {
+
 	}
 	
 	public Scene intializeStartScene() {
@@ -28,14 +31,25 @@ public class GamePlayerView {
 	//	group.getChildren().add(fileBtn);
 		pane.setTop(sampleBar);
 		myScene = setupScene();
+		myScene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
 		return myScene;
 	}
 	
+
 	/**
 	 * 
 	 * @return New scene with the grid, buttons, and a background color
 	 */
 	public Scene setupScene() {
 		return new Scene(pane,WIDTH_SIZE,HEIGHT_SIZE);
+	}
+	
+	private void handleKeyInput (KeyCode code) {
+		if (code == KeyCode.ESCAPE) {
+			Stage mainStage = (Stage) pane.getScene().getWindow();
+			pane.getChildren().get(0).setVisible(false);
+			//mainStage.setScene(new Scene(new Button("asdkl;f")));
+		}
+
 	}
 }
