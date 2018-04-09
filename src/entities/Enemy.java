746c0fa23d98f407e416e34eee86721db69a2b7d
Hemanth Yakkali <hy115@duke.edu>
2018-04-08 22:52:23 -0400

    Finished enemy class, updated entity class

---
+++ src/entities/Enemy.java
@@ -0,0 +1,34 @@
+package entities;
+
+import java.util.Arrays;
+
+import engine.support.ComponentBuilder;
+
+public class Enemy extends Entity{
+	
+	private final String TYPE = "Enemy";
+	private final double ENEMY_DAMAGE = 10;
+	private final double ENEMY_LIFETIME = 50;
+	private final double ENEMY_HEALTH = 100;
+	
+	public Enemy(int ID) {
+		super(ID);
+		addDefaultComponents();
+	}
+
+	@Override
+	public void addDefaultComponents() {
+		this.setHealth(ENEMY_HEALTH);
+		this.setDamage(ENEMY_DAMAGE, ENEMY_LIFETIME);
+		this.setEntityType(TYPE);
+	}
+	
+	public void setVelocity(double xVel, double yVel) {
+		this.add(ComponentBuilder.buildComponent(this.getID(), "Position", Arrays.asList(new String[] {Double.toString(xVel),Double.toString(yVel)})));
+	}
+	
+	public void setDamage(double damage, double lifetime) {
+		this.add(ComponentBuilder.buildComponent(this.getID(), "Damage", Arrays.asList(new String[] {Double.toString(damage),Double.toString(lifetime)})));
+	}
+
+}
