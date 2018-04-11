bfb6349d3619e7bb6f10b6ca45ae0bff19564cdc
Hemanth Yakkali <hy115@duke.edu>
2018-04-11 01:38:19 -0400

    Added buttons for HUD and Global properties

--- src/frontend/components/Level.java
+++ src/frontend/components/Level.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import frontend.entities.Entity;
 
 public class Level {
@@ -15,6 +17,8 @@ public class Level {
 	private String levelText;
 	private double levelTime;
 	private double levelDistance;
+	
+	@XStreamOmitField
 	private List<Entity> entityList;
 	private Map<String,Boolean> HUDprops;
 	private Map<String,String> GProps;
