99be0962706405c142c791a71e0878739906a053
Hemanth Yakkali <hy115@duke.edu>
2018-04-19 15:11:49 -0400

    Removed unused methods and imports

--- src/authoring/views/properties/PropertiesView.java
+++ src/authoring/views/properties/PropertiesView.java
@@ -1,11 +1,5 @@
 package authoring.views.properties;
 
-import java.io.File;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
-
 import authoring.factories.ElementFactory;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -61,53 +55,6 @@ public abstract class PropertiesView {
 	 * @return the title of the window.
 	 */
 	protected abstract String title();
-	
-	/**
-	 * Gets all of the class names from a given package. Useful when determining which properties can be changed.
-	 * @param pckgname name of the package in which to look for these properties
-	 * @return a String array of classes from a given package
-	 */
-	protected String[] getClassesInPackage(String pckgName) {
-        ClassLoader cld = Thread.currentThread().getContextClassLoader();
-        if (cld == null) {
-            throw new IllegalStateException("Can't get class loader.");
-        }
- 
-        URL resource = cld.getResource(pckgName.replace('.', '/'));
-        if (resource == null) {
-            throw new RuntimeException("Package " + pckgName + " not found on classpath.");
-        }
-        File directory = new File(resource.getFile());
-        if (!directory.exists()) {
-            throw new IllegalArgumentException("Could not get directory resource for package " + pckgName + ".");
-        }
-        List<String> classes = new ArrayList<String>();
-        for (String filename : directory.list()) {
-            if (filename.endsWith(".class")) {
-                String classname = buildClassname(pckgName, filename);
-                try {
-                		String clazz = Class.forName(classname).toString();
-                		// Strip everything except for the word following the last period (the actual class name)
-                    classes.add(clazz.substring(clazz.lastIndexOf(".") + 1));
-                } catch (ClassNotFoundException e) {
-                    System.err.println("Error creating class " + classname);
-                }
-            }
-        }
-        return classes.toArray(new String[classes.size()]);
-	}
- 
-	/**
-	 * Builds the class name to fully represent a given class
-	 * @param pckgname the package to look for the class ine
-	 * @param filename the name of the class file
-	 * @return a String representing the fully-qualified class name
-	 */
-    private String buildClassname(String pckgName, String fileName) {
-    		String className = pckgName + '.' + fileName.replace(".class", "");
-        System.out.println(className);
-        return className;
-    }
     
     /**
      * Gets and returns the JavaFX-related root of the {@code PropertiesView}.
