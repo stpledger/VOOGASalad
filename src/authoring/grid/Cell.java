b2e5e0c06fdccffd73ba7816a916dd65711bc1a8
Dylan Powers <ddp19@duke.edu>
2018-04-24 23:30:49 -0400

    Actually fixed merge conflicts

--- src/authoring/grid/Cell.java
+++ src/authoring/grid/Cell.java
@@ -1,17 +1,6 @@
 package authoring.grid;
 
 import authoring.entities.Entity;
-<<<<<<< HEAD
-import authoring.entities.data.EntityLoader;
-import javafx.event.EventHandler;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
-=======
->>>>>>> 50a491971bca15c750a9dabaafd9be2b9e1ae435
 import javafx.scene.layout.Pane;
 
 /**
@@ -24,10 +13,6 @@ import javafx.scene.layout.Pane;
 public class Cell extends Pane {
 
 	private Entity entity;
-<<<<<<< HEAD
-	private Image image;
-=======
->>>>>>> 50a491971bca15c750a9dabaafd9be2b9e1ae435
 	
 	/**
 	 * To initialize a blank cell
@@ -37,51 +22,6 @@ public class Cell extends Pane {
 		this.setPrefWidth(Entity.ENTITY_WIDTH);
 		this.setPrefHeight(Entity.ENTITY_HEIGHT);
 		this.setStyle("-fx-border-color: black");
-<<<<<<< HEAD
-		setupDragActions();
-	}
-
-	/**
-	 * Sets up the drag actions for any cell. 
-	 */
-	private void setupDragActions() {
-		this.setOnDragEntered(e -> this.setStyle("-fx-background-color: #1CFEBA"));
-		this.setOnDragExited(e -> this.setStyle("-fx-border-color: black"));
-		this.setOnDragDetected(e -> {
-			if (this.image != null) {
-				Dragboard db = this.startDragAndDrop(TransferMode.MOVE);
-				ClipboardContent cc = new ClipboardContent();
-				cc.putImage(this.image);
-				db.setContent(cc);
-				e.consume();
-			}
-		});
-		this.setOnDragOver(e -> {
-			if (e.getGestureSource() != this && (e.getDragboard().hasImage() || e.getDragboard().hasString())) {
-				e.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-			}
-			e.consume();
-		});
-		this.setOnDragDropped(e -> {
-			Dragboard db = e.getDragboard();
-			EntityLoader el = new EntityLoader();
-			this.image = db.getImage();
-			ImageView img = new ImageView(db.getImage());
-			img.fitWidthProperty().bind(this.widthProperty());
-			img.fitHeightProperty().bind(this.heightProperty());
-			this.getChildren().add(img);
-			try {
-				el.buildEntity(0, db.getString());
-			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-					| InvocationTargetException | SecurityException | ClassNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			e.setDropCompleted(true);
-			e.consume();
-		});
-=======
->>>>>>> 50a491971bca15c750a9dabaafd9be2b9e1ae435
 	}
 	
 	/**
