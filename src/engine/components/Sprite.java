2b2971214f189d3a90d3f259d9b1f0d463d885b2
Jack Fitzpatrick <jcf44@duke.edu>
2018-04-27 22:45:39 -0400

    Added beginnings of sprite animation

--- src/engine/components/Sprite.java
+++ src/engine/components/Sprite.java
@@ -4,10 +4,14 @@ import java.io.FileNotFoundException;
 
 
 import data.DataRead;
+import engine.systems.SpriteTransition;
 
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
+import javafx.animation.Animation;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 /**
  * Sprite component containing an image. Constructor and setter throw file not found if the filepath is incorrect.
@@ -22,12 +26,15 @@ public class Sprite extends SingleStringComponent {
 
 	@XStreamOmitField
 	private transient ImageView image;
-
+	
+	@XStreamOmitField
+	private transient Animation a;
+	
 	public Sprite(int pid, String path) throws FileNotFoundException {
 	    super(pid, path);
 		setData(path);
 	}
-
+	
 	public ImageView getImage() {
 		if (image == null) setData(getData());
 		return image;
@@ -39,9 +46,30 @@ public class Sprite extends SingleStringComponent {
 		try {
 			image = new ImageView(DataRead.loadImage(im));
 			image.setPickOnBounds(true);
-
+			
 		} catch(RuntimeException e) {
 			System.out.print("Cant load image no Application");
+		}
+		
+	}
+	
+	public void animate(double dur, int count, int columns, int offsetX, int offsetY, int width, int height) {
+		Duration duration = new Duration(dur);
+		getImage().setViewport(new Rectangle2D(offsetX, offsetY, width, height));
+		a = new SpriteTransition(getImage(), duration, count, columns, offsetX, offsetY, width, height);
+		a.setCycleCount(Animation.INDEFINITE);
+		a.play();
+	}
+	
+	public void pauseAnimation() {
+		if(a != null) {
+			a.pause();
+		}
+	}
+	
+	public void playAnimation() {
+		if(a != null) {
+			a.play();			
 		}
 	}
 	
