b5af344395313c1f947fd932a8c749670bb21172
Ryan Fu <rwf11@duke.edu>
2018-04-28 21:44:39 -0400

    ready to merge with master

--- src/GamePlayer/HighScoreView.java
+++ src/GamePlayer/HighScoreView.java
@@ -32,8 +32,6 @@ public class HighScoreView extends BranchScreenView {
 	private Double finalScore;
 	private String finalGameName;
 	
-	//FaceBook Client Variables:
-	private String appID = "190769674886367";
 	//Data Structure to Display High Scores
 	private ObservableList<Person> data;
 	
