e65f70fe85af344845b40a50004ca40aab2ee1f6
Hemanth Yakkali <hy115@duke.edu>
2018-04-29 17:11:47 -0400

    Added images

--- src/authoring/views/EntityBuilderView.java
+++ src/authoring/views/EntityBuilderView.java
@@ -88,7 +88,7 @@ public class EntityBuilderView extends Stage {
 			this.saveMenu = buildSingleButtonMenu("save", e -> {save();});
 			addImageMenu = buildSingleButtonMenu("addImage", e -> {addImage();});
 			this.entityPreview = new ImageView();
-			updateEntityPreview(new Image("games/DemoDemo/data/images/no_image.jpg"));
+			updateEntityPreview(new Image("data/images/no_image.jpg"));
 			this.root.getChildren().addAll(entityPreview, typeComboBox, addImageMenu, componentFormCollection, saveMenu);
 			this.root.getStyleClass().add("entity-builder-view");
 		}  catch (Exception e) {
