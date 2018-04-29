2a6c17792ab1aae7060d2f45a483f49dca799621
Charles <cnd11@duke.edu>
2018-04-29 02:44:39 -0400

    White remover from images utility

---
+++ src/data/ImageNoWhite.java
@@ -0,0 +1,33 @@
+package data;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class ImageNoWhite {
+
+    public java.awt.Image convertAWT(java.awt.Image image) {
+        BufferedImage original = (BufferedImage) image;
+        BufferedImage whiteFilter = new BufferedImage(original.getWidth(),
+                original.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g = whiteFilter.createGraphics();
+        for (int i = 0; i < whiteFilter.getWidth(); i++) {
+            for (int j = 0; j < whiteFilter.getHeight(); j++) {
+                if (original.getRGB(i, j) != Color.WHITE.getRGB()) {
+                    whiteFilter.setRGB(i, j, original.getRGB(i, j));
+                }
+            }
+        }
+        g.dispose();
+
+        return whiteFilter;
+    }
+
+    public Image convert(Image image) {
+        BufferedImage whiteFilter = SwingFXUtils.fromFXImage(image, null);
+        return SwingFXUtils.toFXImage((BufferedImage) convertAWT(whiteFilter), null);
+    }
+}
