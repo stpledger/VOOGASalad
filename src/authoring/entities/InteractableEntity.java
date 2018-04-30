7c3981b742c1905c9f3d488aff0b5d1069023d4f
Collin Brown <cdb55@duke.edu>
2018-04-29 23:50:05 -0400

    dunno

--- src/authoring/entities/InteractableEntity.java
+++ src/authoring/entities/InteractableEntity.java
@@ -75,14 +75,14 @@ public abstract class InteractableEntity extends Entity {
 	public void add(Component c) {
 		// component must be in the list of addable components
 
-		//if (ResourceBundle.getBundle(I_PROPERTIES_PATH).keySet().contains(c.getKey())) {
+		if (ResourceBundle.getBundle(I_PROPERTIES_PATH).keySet().contains(c.getKey())) {
         		if (c != null) {
         			if (this.contains(c)) {
         				this.removeByName(c.getKey());
         			}
         			this.components.add(c);
         		}
-		//}
+		}
 	}
 	
 	/**
