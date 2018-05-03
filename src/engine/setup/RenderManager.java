fb9c2148ca48ae9aa7db8623f8fb579a69305a60
Stefani Vukajlovic <sv116@duke.edu>
2018-05-03 15:47:03 -0400

    changes to images

--- src/engine/setup/RenderManager.java
+++ src/engine/setup/RenderManager.java
@@ -89,6 +89,7 @@ public class RenderManager {
     }
 
     private boolean withinRenderDistance(double x, double y) {
-        return Math.abs(centerX - x) < renderDistance && Math.abs(centerY - y) < renderDistance;
+        return true;
+        		//Math.abs(centerX - x) < renderDistance && Math.abs(centerY - y) < renderDistance;
     }
 }
