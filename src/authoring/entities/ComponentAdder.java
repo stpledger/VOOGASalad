6ddb5ee406f7a35207a1f178dc9c0d0c8e5e1fe4
Conrad Mitchell <cmm134@duke.edu>
2018-05-03 19:34:24 -0400

    Adding small changes

--- src/authoring/entities/ComponentAdder.java
+++ src/authoring/entities/ComponentAdder.java
@@ -29,7 +29,7 @@ public class ComponentAdder {
 
                 Group root = new Group();
 				try {
-					root = new Group(new PropertiesComponentForm(entity.getID(),component));
+					root = new Group(new PropertiesComponentForm(entity.getID(),component, null));
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
